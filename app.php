f68232aeabe888c6a79a8d9d7747c570cf35888b
ancyra2 <duran.talih@gmail.com>
2021-08-17 11:18:46 +0300

    Ödev istenilen şekilde yapıldı.

--- app.php
+++ app.php
@@ -1,6 +1,25 @@
 <?php
 
-$age = 25;
+$age = 24;
+
+/**
+ * Problemde sadece "Çocuk", "Genç", "Yetişkin", "Yaşlı" dizgelerinin
+ * yazdırılması istenmesi sebebiyle ve değişkenin değerinin dinamik bir
+ * şekilde değiştirilmesi istenilmediğinden $age değişkeninin
+ * negatif olması ve çok yüksek yaş değerleri içermesi
+ * durumlarıyla ilgilenilmemiştir.
+ */
+
+
+if ($age >= 0 && $age <= 14) {
+    echo "Çocuk";
+} elseif ($age > 14 && $age <= 24) {
+    echo "Genç";
+} elseif ($age > 24 && $age <= 64) {
+    echo "Yetişkin";
+} elseif ($age > 64) {
+    echo "Yaşlı";
+}
 
 /**
  * Bu ödevde age değişkeni içerisinde yer alan yaş değerinin bulunduğu
